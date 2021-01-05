bdd6d92899497d1b0a3f2ea669678c95e7f573bb
増井 悠太 <yuta970702m@gmail.com>
2021-01-05 22:18:40 +0900

    start chartjs
    start chartjs & sample

---
+++ components/LineChart.vue
@@ -0,0 +1,29 @@
+<script>
+import { Line } from 'vue-chartjs'
+export default {
+  extends: Line,
+  data(){
+    return {
+        chartdata: {
+            labels: [1,2,3],
+            datasets: [
+                {
+                lineTension: 0, 
+                // 直線的な推移
+                label: ['勝ち点推移'],
+                backgroundColor: '#f87979',
+                data: [0, 30,100]
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
